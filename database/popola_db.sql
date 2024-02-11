8392fa3dd9fc92dd4f06016773491ecf0268b102
igor <svaraigorw@gmail.com>
2024-02-11 02:06:10 +0100

    migliorata la struttura di popolamento con un 'do' e parametrizzata

--- database/popola_db.sql
+++ database/popola_db.sql
@@ -10,15 +10,15 @@ DELETE FROM SPECIFICHE_TECNICHE WHERE TRUE;
 
 -- Popolo le tabelle con i csv
 DO $$
-DECLARE
-    common_path TEXT := '<la/tua/path>/Voli_aeroporto/database/TABLES/';
-BEGIN
-    EXECUTE 'COPY EQUIPAGGIO (id_equipaggio) FROM ' || quote_literal(common_path || 'EQUIPAGGIO.csv') || ' DELIMITER '','' CSV HEADER;';
-    EXECUTE 'COPY STEWARD (codice_fiscale, id_equipaggio) FROM ' || quote_literal(common_path || 'STEWARD.csv') || ' DELIMITER '','' CSV HEADER;';
-    EXECUTE 'COPY HOSTESS (codice_fiscale, id_equipaggio) FROM ' || quote_literal(common_path || 'HOSTESS.csv') || ' DELIMITER '','' CSV HEADER;';
-    EXECUTE 'COPY PILOTA (codice_fiscale, id_equipaggio) FROM ' || quote_literal(common_path || 'PILOTA.csv') || ' DELIMITER '','' CSV HEADER;';
-    EXECUTE 'COPY SPECIFICHE_TECNICHE (peso, lunghezza, apertura_alare) FROM ' || quote_literal(common_path || 'SPECIFICHE_TECNICHE.csv') || ' DELIMITER '','' CSV HEADER;';
-    EXECUTE 'COPY MODELLO (nome_modello, azienda_costruttrice, carico_max, persone_max, peso, lunghezza, apertura_alare) FROM ' || quote_literal(common_path || 'MODELLO.csv') || ' DELIMITER '','' CSV HEADER;';
-    EXECUTE 'COPY AEROMOBILE (id_aereo, nome_modello, azienda_costruttrice) FROM ' || quote_literal(common_path || 'AEROMOBILE.csv') || ' DELIMITER '','' CSV HEADER;';
-    EXECUTE 'COPY VOLO (gate, ora, destinazione, id_equipaggio, id_aereo) FROM ' || quote_literal(common_path || 'VOLO.csv') || ' DELIMITER '','' CSV HEADER;';
-END $$;
+    DECLARE
+        common_path TEXT := '<la/tua/path>/Voli_aeroporto/database/TABLES/';
+    BEGIN
+        EXECUTE 'COPY EQUIPAGGIO (id_equipaggio) FROM ' || quote_literal(common_path || 'EQUIPAGGIO.csv') || ' DELIMITER '','' CSV HEADER;';
+        EXECUTE 'COPY STEWARD (codice_fiscale, id_equipaggio) FROM ' || quote_literal(common_path || 'STEWARD.csv') || ' DELIMITER '','' CSV HEADER;';
+        EXECUTE 'COPY HOSTESS (codice_fiscale, id_equipaggio) FROM ' || quote_literal(common_path || 'HOSTESS.csv') || ' DELIMITER '','' CSV HEADER;';
+        EXECUTE 'COPY PILOTA (codice_fiscale, id_equipaggio) FROM ' || quote_literal(common_path || 'PILOTA.csv') || ' DELIMITER '','' CSV HEADER;';
+        EXECUTE 'COPY SPECIFICHE_TECNICHE (peso, lunghezza, apertura_alare) FROM ' || quote_literal(common_path || 'SPECIFICHE_TECNICHE.csv') || ' DELIMITER '','' CSV HEADER;';
+        EXECUTE 'COPY MODELLO (nome_modello, azienda_costruttrice, carico_max, persone_max, peso, lunghezza, apertura_alare) FROM ' || quote_literal(common_path || 'MODELLO.csv') || ' DELIMITER '','' CSV HEADER;';
+        EXECUTE 'COPY AEROMOBILE (id_aereo, nome_modello, azienda_costruttrice) FROM ' || quote_literal(common_path || 'AEROMOBILE.csv') || ' DELIMITER '','' CSV HEADER;';
+        EXECUTE 'COPY VOLO (gate, ora, destinazione, id_equipaggio, id_aereo) FROM ' || quote_literal(common_path || 'VOLO.csv') || ' DELIMITER '','' CSV HEADER;';
+    END $$;
