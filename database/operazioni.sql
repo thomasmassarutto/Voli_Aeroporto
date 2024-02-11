44975bc2ac1304a1c8b6ab83bad8257a67241c45
igor <svaraigorw@gmail.com>
2024-02-11 02:13:00 +0100

    create tre funzioni, la terza funzione non e' ancora completata, da rivedere

--- database/operazioni.sql
+++ database/operazioni.sql
@@ -1,11 +1,5 @@
--- Clean up delle funzioni
-DROP FUNCTION IF EXISTS Ricerca_Voli_Destinazione;
-DROP FUNCTION IF EXISTS Steward_Aerei_Pesanti;
-DROP FUNCTION IF EXISTS Aerei_Di_Linea;
-
-
 -- Creazione delle funzioni
-CREATE FUNCTION Ricerca_Voli_Destinazione(destinazione_desiderata VARCHAR)
+CREATE OR REPLACE FUNCTION Ricerca_Voli_Destinazione(destinazione_desiderata VARCHAR)
     RETURNS TABLE (
         gate int,
         ora varchar,
@@ -23,25 +17,27 @@ END;
 $$ language plpgsql;
 
 
-CREATE FUNCTION Steward_Aerei_Pesanti(X INT, Y INT)
-RETURNS INT AS $$
+CREATE OR REPLACE FUNCTION Steward_Aerei_Pesanti(X INT, Y INT)
+    RETURNS INT AS $$
 DECLARE
     num_steward INT;
 BEGIN
-    SELECT COUNT(DISTINCT s.id) INTO num_steward
+    SELECT COUNT(DISTINCT(s.codice_fiscale)) INTO num_steward
     FROM STEWARD s
-        JOIN EQUIPAGGIO e ON s.id_equipaggio = e.id_equipaggio
-        JOIN VOLO v ON e.id_equipaggio = v.id_equipaggio
-        JOIN AEREO a ON v.id_ereo = a.id_aereo
-    WHERE a.peso BETWEEN X and Y;
-    GROUP BY s.id;
+        -- quando gli attributi con cui fare join hanno lo stesso nome si puo' usare USING
+        JOIN EQUIPAGGIO e USING (id_equipaggio)
+        JOIN VOLO v USING (id_equipaggio)
+        JOIN AEROMOBILE a USING (id_aereo)
+        JOIN MODELLO m USING (nome_modello, azienda_costruttrice)
+    WHERE m.peso BETWEEN 100000 and 150000;
 
     RETURN num_steward;
 END;
 $$ LANGUAGE plpgsql;
 
 
-CREATE FUNCTION Aerei_Di_Linea()
+-- TODO: Verificare se funziona e correggeri evenntuali errori
+CREATE OR REPLACE FUNCTION Aerei_Di_Linea()
 RETURNS TABLE (
     id_aereo INT,
     nome_modello VARCHAR(255),
@@ -53,13 +49,24 @@ RETURNS TABLE (
     ) AS $$
 BEGIN
     RETURN QUERY
--- TODO: come seleziono il minimo?
-    SELECT a.id, a.nome_modello, a.azienda_costruttrice, MIN(a.persone_max), a.peso, a.lunghezza, a.apertura_alare
-    FROM AEROMOBILE a
-        JOIN VOLO v ON v.id_aereo = a.id_aereo
-        JOIN EQUIPAGGIO e ON e.id_equipaggio = v.id_equipaggio
-        JOIN PILOTA p ON e.id_equipaggio = p.id_equipaggio
-    WHERE p.età BETWEEN 30 AND 60
+        SELECT *
+        FROM (
+            SELECT
+                a.id_aereo,
+                a.nome_modello,
+                a.azienda_costruttrice,
+    --             a.persone_max,
+    --             a.peso,
+    --             a.lunghezza,
+    --             a.apertura_alare,
+                RANK() OVER (ORDER BY a.persone_max ASC) AS ranking
+            FROM AEROMOBILE a
+                JOIN VOLO v USING (id_equipaggio)
+                JOIN EQUIPAGGIO e USING (id_equipaggio)
+                JOIN PILOTA p USING (id_equipaggio)
+            WHERE p.eta BETWEEN 30 AND 60)
+    --             WHERE p.età BETWEEN 30 AND 60 AND ranking = 1) TODO: opzione 2
+        WHERE ranking = 1;
 END;
 $$ LANGUAGE plpgsql;
 
