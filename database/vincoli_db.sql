5c4ea2aa2974629be890197e4af1eb601b16dc70
igor <svaraigorw@gmail.com>
2024-02-17 23:12:21 +0100

    terminati tutti i vincoli con tutti i trigger

--- database/vincoli_db.sql
+++ database/vincoli_db.sql
@@ -34,24 +34,31 @@ ALTER TABLE SPECIFICHE_TECNICHE
 
 
 
--- TRIGGER
-DROP TRIGGER IF EXISTS trigger_insert_update_equipaggio ON EQUIPAGGIO;
+-- Clean up TRIGGERS
+DROP TRIGGER IF EXISTS trigger_atleast_one ON EQUIPAGGIO;
 DROP TRIGGER IF EXISTS trigger_delete_hostess ON HOSTESS;
 DROP TRIGGER IF EXISTS trigger_delete_steward ON STEWARD;
+DROP TRIGGER IF EXISTS trigger_exact_two ON EQUIPAGGIO;
+DROP TRIGGER IF EXISTS trigger_nomore_two_piloti ON PILOTA;
+DROP TRIGGER IF EXISTS trigger_exists_volo ON VOLO;
+DROP TRIGGER IF EXISTS trigger_valid_num_equipaggio ON VOLO;
+
+
 
 
 --  VINCOLO tra EQUIPAGGIO, HOSTESS e STEWARD
 --      - equipaggio non puo' essere inserito senza hostess oppure steward
 --      - equipaggio deve avere sempre almeno un hostess oppure uno steward (quindi se viene eliminato anche l'ultimo S o H dall'equipaggio questo viene eliminato di conseguenza)
---  ND.: la chiave esterna in hostess e steward garantisce:
+--  NB.: la chiave esterna in hostess e steward (verso equipaggio) garantisce:
 --      - non puo' esistere hostess o steward senza equipaggio
 --      - equipaggio non puo' essere eliminato se ha qualche hostess o steward associati
-CREATE OR REPLACE FUNCTION trigger_function_insert_update_equipaggio()
+
+CREATE OR REPLACE FUNCTION trigger_function_atleast_one()
     RETURNS TRIGGER AS $$
 BEGIN
     IF NOT (
-                NEW.id_equipaggio IN (SELECT id_equipaggio FROM HOSTESS) OR
-                NEW.id_equipaggio IN (SELECT id_equipaggio FROM STEWARD))
+            NEW.id_equipaggio IN (SELECT id_equipaggio FROM HOSTESS) OR
+            NEW.id_equipaggio IN (SELECT id_equipaggio FROM STEWARD))
     THEN
         RAISE EXCEPTION 'INSERT/UPDATE FAILED: id_equipaggio deve avere almeno uno tra HOSTESS o STEWARD';
     END IF;
@@ -77,10 +84,10 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER trigger_insert_update_equipaggio
+CREATE TRIGGER trigger_atleast_one
     BEFORE INSERT OR UPDATE ON EQUIPAGGIO
     FOR EACH ROW
-EXECUTE FUNCTION trigger_function_insert_update_equipaggio();
+EXECUTE FUNCTION trigger_function_atleast_one();
 
 CREATE TRIGGER trigger_delete_hostess
     AFTER DELETE ON HOSTESS
@@ -92,162 +99,192 @@ CREATE TRIGGER trigger_delete_steward
     FOR EACH ROW
 EXECUTE FUNCTION trigger_function_delete_hostess_steward();
 
--- FUNZIONI per L'INSERIMENTO E LA CANCELLAZIONE DI HOSTESS/STEWARD/EQUIPAGGIO
+
+-- FUNZIONI per L'INSERIMENTO DI HOSTESS/STEWARD/EQUIPAGGIO
 --  Inserimento:
 --      La tabella hostess e steward hanno "DEFERRABLE" sulle chiavi esterne di moda da poterle ritardare al termine della transazione
 --      Per inserire un equipaggio vanno associati almeno un hostess oppure uno steward, con questa procedura lo si puo' fare comodamente
---      NB.: L'utente deve usare una transazione impostando i due vincoli di chiave esterna a "DEFERRED"
+--      L'utente deve usare una transazione impostando i due vincoli di chiave esterna a "DEFERRED"
 
--- esempio di utilizzo per inserire un equipaggio con uno steward
-START TRANSACTION;
-SET CONSTRAINTS fk_hos_equipaggio, fk_stw_equipaggio DEFERRED;
-CALL insert_equipaggio_with_H_S('E01', NULL, 'SRLSDTL95E62F631I');
-COMMIT;
+--  esempio di utilizzo:
+-- START TRANSACTION;
+-- SET CONSTRAINTS fk_hos_equipaggio, fk_stw_equipaggio DEFERRED;
+-- INSERT INTO HOSTESS (codice_fiscale, id_equipaggio) VALUES ('STLFSC87E62L491I', 'E01');
+-- INSERT INTO STEWARD (codice_fiscale, id_equipaggio) VALUES ('SRLSDT95E62F631I', 'E01');
+-- INSERT INTO EQUIPAGGIO (id_equipaggio) VALUES ('E01');
+-- COMMIT;
 
-CREATE OR REPLACE PROCEDURE insert_equipaggio_with_H_S(
-    id_equipaggio VARCHAR(255),
-    codice_fiscale_hostess CHAR(16),
-    codice_fiscale_steward CHAR(16)
+
+
+
+
+
+
+--  VINCOLO tra EQUIPAGGIO e PILOTA
+--      - equipaggio non puo' essere inserito senza esattamente due piloti associati
+--      - un pilota non puo' essere inserito se l'equipaggio possiede gia' due
+--  NB.: la chiave esterna in pilota (verso equipaggio) garantisce:
+--      - non puo' esistere un pilota senza equipaggio
+--      - equipaggio non puo' essere eliminato se ha un qualche pilota associato
+
+CREATE OR REPLACE FUNCTION trigger_function_exact_two()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF (
+        SELECT COUNT(DISTINCT p.codice_fiscale)
+        FROM PILOTA p
+        WHERE p.id_equipaggio = NEW.id_equipaggio
+    ) != 2 THEN
+        RAISE EXCEPTION 'INSERT/UPDATE FAILED: L''equipaggio deve avere esattamente due piloti.';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_exact_two
+    BEFORE INSERT OR UPDATE ON EQUIPAGGIO
+    FOR EACH ROW
+EXECUTE FUNCTION trigger_function_exact_two();
+
+
+CREATE OR REPLACE FUNCTION trigger_function_nomore_two_piloti()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF (
+       SELECT COUNT(DISTINCT p.codice_fiscale)
+       FROM PILOTA p
+       WHERE p.id_equipaggio = NEW.id_equipaggio
+    ) >= 2 THEN
+        RAISE EXCEPTION 'INSERT/UPDATE FAILED: L''equipaggio possiede gia'' due piloti';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Creazione di un trigger per il secondo controllo
+CREATE TRIGGER trigger_nomore_two_piloti
+    BEFORE INSERT OR UPDATE ON PILOTA
+    FOR EACH ROW
+EXECUTE FUNCTION trigger_function_nomore_two_piloti();
+
+-- FUNZIONI per L'INSERIMENTO DI PILOTA/EQUIPAGGIO
+--  Inserimento:
+--      La tabella pilota ha "DEFERRABLE" sulla chiave esterne di moda da poterle ritardare al termine della transazione
+--      Per inserire un equipaggio vanno associati esattamente due piloti, con questa procedura lo si puo' fare comodamente
+--      L'utente deve usare una transazione impostando il vincolo di chiave esterna a "DEFERRED"
+
+--  esempio di utilizzo:
+-- START TRANSACTION;
+-- SET CONSTRAINTS fk_plt_equipaggio DEFERRED;
+-- INSERT INTO PILOTA (codice_fiscale, id_equipaggio) VALUES ('ZLCSSC93A69I530I', 'E01');
+-- INSERT INTO PILOTA (codice_fiscale, id_equipaggio) VALUES ('TSDDCN86A69I530I', 'E01');
+-- INSERT INTO EQUIPAGGIO (id_equipaggio) VALUES ('E01');
+-- COMMIT;
+
+
+
+
+
+
+--  VINCOLO tra EQUIPAGGIO e VOLO
+--      - equipaggio non puo' essere inserito senza un volo associato
+--  NB.: la chiave esterna in volo (verso equipaggio) garantisce:
+--      - non puo' esistere volo senza equipaggio
+--      - equipaggio non puo' essere eliminato se ha un qualche volo associato
+
+CREATE OR REPLACE FUNCTION trigger_function_exists_volo()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM VOLO
+        WHERE id_equipaggio = NEW.id_equipaggio
+    ) THEN
+        RAISE EXCEPTION 'INSERT/UPDATE FAILED: Nessun volo associato all''equipaggio';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_exists_volo
+    BEFORE INSERT OR UPDATE ON EQUIPAGGIO
+    FOR EACH ROW
+EXECUTE FUNCTION trigger_function_exists_volo();
+
+-- FUNZIONI per L'INSERIMENTO DI VOLO/EQUIPAGGIO
+--  Inserimento:
+--      La tabella volo ha "DEFERRABLE" sulla chiave esterne di moda da poterle ritardare al termine della transazione
+--      Per inserire un equipaggio va associato un volo, con questa procedura lo si puo' fare comodamente
+--      L'utente deve usare una transazione impostando il vincolo di chiave esterna a "DEFERRED"
+--      L'aereo deve essere gia' presente nel database per non violare il vincolo di chiave esterna su "id_aereo"
+
+--  esempio di utilizzo:
+-- START TRANSACTION;
+-- SET CONSTRAINTS fk_volo_equipaggio DEFERRED;
+-- INSERT INTO VOLO (gate, ora, destinazione, id_equipaggio, id_aereo)
+--      VALUES (1,	'07:00:00',	'Porto','E01','A01');
+-- INSERT INTO EQUIPAGGIO (id_equipaggio) VALUES ('E01');
+-- COMMIT;
+
+
+
+
+
+
+
+
+
+
+-- INSERTION PROCEDURE
+-- - Per un inserimento piu' facile usiamo la seguente procedura
+-- - Bisogna assicurarsi che la procedura sia interna ad una transazione che temporaneamente ritarda dei vincoli di chiave esterna specifici
+
+-- esempio di utilizzo:
+-- START TRANSACTION;
+-- SET CONSTRAINTS fk_hos_equipaggio, fk_stw_equipaggio, fk_plt_equipaggio, fk_volo_equipaggio DEFERRED;
+-- CALL insert_volo_con_personale(
+--     'E01',
+--     'STLFSC87E62L491I',
+--     'SRLSDT95E62F631I',
+--     'ZLCSSC93A69I530I',
+--     'TSDDCN86A69I530I',
+--     1, '07:30:00', 'Milano',
+--     'A1'
+-- );
+-- COMMIT;
+
+CREATE OR REPLACE PROCEDURE insert_volo_con_personale(
+    id_equipaggio           VARCHAR(255),
+    codice_fiscale_hostess  CHAR(16),
+    codice_fiscale_steward  CHAR(16),
+    codice_fiscale_pilota1  CHAR(16),
+    codice_fiscale_pilota2  CHAR(16),
+    gate                    INT,
+    ora                     TIME,
+    destinazione            VARCHAR,
+    id_aereo                VARCHAR
 )
 AS $$
 BEGIN
-    -- L'inserimento di hostess e/o steward sono fatti prima dell'inserimento di equipaggio per non violare il vincolo di chiave esterna
+    -- hostess e/o steward possono essere NULL (necessario uno dei due)
     IF codice_fiscale_hostess IS NOT NULL THEN
-        INSERT INTO HOSTESS (codice_fiscale, id_equipaggio) VALUES (codice_fiscale_hostess, id_equipaggio);
+        INSERT INTO HOSTESS VALUES (codice_fiscale_hostess, id_equipaggio);
     END IF;
     IF codice_fiscale_steward IS NOT NULL THEN
-        INSERT INTO STEWARD (codice_fiscale, id_equipaggio) VALUES (codice_fiscale_steward, id_equipaggio);
+        INSERT INTO STEWARD VALUES (codice_fiscale_steward, id_equipaggio);
     END IF;
 
-    INSERT INTO EQUIPAGGIO (id_equipaggio) VALUES (id_equipaggio);
+    INSERT INTO PILOTA (codice_fiscale, id_equipaggio)
+        VALUES (codice_fiscale_pilota1, id_equipaggio);
+    INSERT INTO PILOTA (codice_fiscale, id_equipaggio)
+        VALUES (codice_fiscale_pilota2, id_equipaggio);
+
+    INSERT INTO VOLO VALUES (gate, ora, destinazione, id_equipaggio, id_aereo);
+
+    INSERT INTO EQUIPAGGIO VALUES (id_equipaggio);
 END;
 $$ LANGUAGE plpgsql;
-
-
-
-
-
--- TODO: STAGING AREA
-
--- TODO: STAGING AREA
-
-
-
--- VINCOLI
--- (RV2) Il numero di persone che compone l'equipaggio deve essere minore o uguale al numero massimo di persone trasportabili dall'aeromobile con cui volano.
-CREATE FUNCTION check_numero_equipaggio()
-    RETURNS TRIGGER AS $$
-BEGIN
-    IF ((SELECT persone_max
-        FROM AEROMOBILE a
-            JOIN MODELLO USING (nome_modello, azienda_costruttrice)
-            WHERE a.id_aereo = NEW.id_aereo
-        ) < (
-        SELECT COUNT(*)
-        FROM EQUIPAGGIO e
-            JOIN HOSTESS h ON e.id_equipaggio = h.id_equipaggio
-            JOIN STEWARD s ON e.id_equipaggio = s.id_equipaggio
-            JOIN PILOTA p ON e.id_equipaggio = p.id_equipaggio
-        WHERE e.id_equipaggio = NEW.id_equipaggio ))
-    THEN
-        RAISE EXCEPTION 'Il numero di membri dell''equipaggio supera la capacità dell''aereo.';
-    END IF;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER check_numero_equipaggio_trigger
-    BEFORE INSERT OR UPDATE
-    ON VOLO
-    FOR EACH ROW
-    EXECUTE FUNCTION check_numero_equipaggio();
-
-
--- ALTER TABLE VOLO ADD CONSTRAINT ck_numero_equipaggio
---     CHECK ((
---         SELECT persone_max
---          FROM AEROMOBILE a
---               JOIN MODELLO USING (nome_modello, azienda_costruttrice)
---          WHERE a.id_aereo = VOLO.id_aereo
---         ) >= (
---         SELECT COUNT(*)
---         FROM EQUIPAGGIO e
---              JOIN HOSTESS h ON e.id_equipaggio = h.id_equipaggio
---              JOIN STEWARD s ON e.id_equipaggio = s.id_equipaggio
---              JOIN PILOTA p ON e.id_equipaggio = p.id_equipaggio
---         WHERE e.id_equipaggio = VOLO.id_equipaggio
---         ));
-
-
-
-
-
--- VINCOLI SU EQUIPAGGIO
-ALTER TABLE EQUIPAGGIO
-    ADD CONSTRAINT ck_equipaggio_volo
-        CHECK (
-            EXISTS (SELECT *
-                    FROM VOLO
-                    WHERE id_equipaggio = EQUIPAGGIO.id_equipaggio)
-            );
-
-ALTER TABLE EQUIPAGGIO
-    ADD CONSTRAINT fk_equipaggio_hostess_steward
-        CHECK (
-            (id_equipaggio IN (SELECT id_equipaggio FROM HOSTESS)) OR
-            (id_equipaggio IN (SELECT id_equipaggio FROM STEWARD))
-            );
-
-ALTER TABLE EQUIPAGGIO
-    ADD CONSTRAINT fk_equipaggio_piloti
-        CHECK (
-            (SELECT COUNT(DISTINCT(p.codice_fiscale))
-            FROM PILOTA p
-            WHERE p.id_equipaggio = EQUIPAGGIO.id_equipaggio)
-            = 2
-            );
-
--- TODO: versione alternativa
--- ALTER TABLE EQUIPAGGIO
---     ADD CONSTRAINT ck_equipaggio_due_piloti
---         CHECK (
---             (EXISTS (
---                 SELECT *
---                 FROM PILOTA p1, PILOTA p2
---                 WHERE p1.codice_fiscale <> p2.codice_fiscale
---                   AND EQUIPAGGIO.id_equipaggio = p1.id_equipaggio = p2.id_equipaggio
---                 ))
---             AND
---             (NOT EXISTS (
---                     SELECT *
---                     FROM PILOTA p1, PILOTA p2, PILOTA p3
---                     WHERE p1.codice_fiscale <> p2.codice_fiscale <> p3.codice_fiscale
---                       AND EQUIPAGGIO.id_equipaggio = p1.id_equipaggio = p2.id_equipaggio = p3.id_equipaggio
---                 ))
---             );
-
-
-
-
-
-
-
--- TODO: Versione con il TRIGGER
-CREATE OR REPLACE FUNCTION check_equipaggio_integrity()
-    RETURNS TRIGGER AS $$
-BEGIN
-    IF ((id_equipaggio NOT IN (SELECT id_equipaggio FROM HOSTESS)) OR
-        (id_equipaggio IN (SELECT id_equipaggio FROM STEWARD))) THEN
-        RAISE EXCEPTION 'Violazione del vincolo di integrità: l''id_equipaggio deve essere presente in HOSTESS o STEWARD.';
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER equipaggio_integrity_trigger
-    BEFORE INSERT OR UPDATE
-    ON EQUIPAGGIO
-    FOR EACH ROW
-    EXECUTE FUNCTION check_equipaggio_integrity();
 
