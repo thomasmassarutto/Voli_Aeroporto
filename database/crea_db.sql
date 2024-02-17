8c2fe7c1acde21a9955a05204da6de278aaa7a73
igor <svaraigorw@gmail.com>
2024-02-18 00:58:49 +0100

    migliorata la struttura con l'attributo derivato

--- database/crea_db.sql
+++ database/crea_db.sql
@@ -79,64 +79,37 @@ CREATE TABLE VOLO
     gate          INT,
     ora           TIME,
     destinazione  VARCHAR(255)                                              NOT NULL,
+    capacita_passeggeri INT,
     id_equipaggio VARCHAR(255) REFERENCES EQUIPAGGIO (id_equipaggio) UNIQUE NOT NULL,
     id_aereo      VARCHAR(255) REFERENCES AEROMOBILE (id_aereo) UNIQUE      NOT NULL,
     PRIMARY KEY (gate, ora)
 );
 
--- Soluzione per capacita' passeggeri
-CREATE VIEW VOLO_COMPLETO AS
-SELECT
-    v.gate,
-    v.ora,
-    v.destinazione,
-    (
+CREATE OR REPLACE FUNCTION trigger_function_calcola_capacita_passeggeri()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.capacita_passeggeri := (
         SELECT m.persone_max
-        FROM VOLO v1
-            JOIN AEROMOBILE a USING (id_aereo)
+        FROM AEROMOBILE a
             JOIN MODELLO m USING (nome_modello, azienda_costruttrice)
-        WHERE v1.gate = v.gate AND v1.ora = v.ora
+        WHERE a.id_aereo = NEW.id_aereo
     ) - 2 - (
         SELECT COUNT(*)
-        FROM VOLO v2
-            JOIN EQUIPAGGIO e USING (id_equipaggio)
-            JOIN HOSTESS h USING (id_equipaggio)
-            JOIN STEWARD s USING (id_equipaggio)
-        WHERE v2.gate = v.gate AND v2.ora = v.ora
-    ) AS capacita_passeggeri,
-    v.id_equipaggio,
-    v.id_aereo
-FROM VOLO v;
+        FROM HOSTESS h
+            JOIN EQUIPAGGIO USING (id_equipaggio)
+        WHERE h.id_equipaggio = NEW.id_equipaggio
+    ) + (
+        SELECT COUNT(*)
+        FROM STEWARD s
+            JOIN EQUIPAGGIO USING (id_equipaggio)
+        WHERE s.id_equipaggio = NEW.id_equipaggio
+    );
 
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
-
--- TODO: vedi se si puo' implementare la capacita' passeggeri con un trigger
--- CREATE FUNCTION calcola_cap_passeggeri()
---     RETURNS TRIGGER AS $$
--- BEGIN
---     NEW.capacita_passeggeri :=
---         -- capacita' massima passeggeri dell'aereo
---         (SELECT m.persone_max
---          FROM VOLO v
---                   JOIN AEROMOBILE a USING (id_aereo)
---                   JOIN MODELLO m USING (nome_modello, azienda_costruttrice)
---          WHERE v.gate = NEW.gate AND v.ora = NEW.ora )
---         -- sempre due piloti
---         - 2 -
---         -- conta il numero degli assistenti
---         (SELECT COUNT (*)
---         FROM VOLO v
---             JOIN EQUIPAGGIO e ON v.id_equipaggio = e.id_equipaggio
---             JOIN HOSTESS h ON e.id_equipaggio = h.id_equipaggio
---             JOIN STEWARD s ON e.id_equipaggio = s.id_equipaggio
---         WHERE v.gate = NEW.gate AND v.ora = NEW.ora );
---
---     RETURN NEW;
--- END;
--- $$ LANGUAGE plpgsql;
---
--- CREATE TRIGGER deriva_cap_passeggeri
---     AFTER INSERT OR UPDATE
---     ON VOLO
---     FOR EACH ROW
---     EXECUTE FUNCTION calcola_cap_passeggeri();
+CREATE TRIGGER trigger_calcola_capacita_passeggeri
+    BEFORE INSERT OR UPDATE ON VOLO
+    FOR EACH ROW
+EXECUTE FUNCTION trigger_function_calcola_capacita_passeggeri();
